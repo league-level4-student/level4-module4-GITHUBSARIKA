a1a6c4cc08f31328635bd0d05cceccf2c5ab37f8
root <root@b8d9ad9e3dfc>
2020-04-10 01:26:57 +0000

    finished step 6

--- src/_03_polymorphs/PolymorphWindow.java
+++ src/_03_polymorphs/PolymorphWindow.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -16,10 +17,7 @@ public class PolymorphWindow extends JPanel implements ActionListener{
     
     private JFrame window;
     private Timer timer;
-    
-    Polymorph bluePoly;
-    Polymorph redmorph;
-    Polymorph mmorph;
+   ArrayList<Polymorph> polymorph;
     public static void main(String[] args) {
    	 new PolymorphWindow().buildWindow();
     }
@@ -32,31 +30,37 @@ public class PolymorphWindow extends JPanel implements ActionListener{
    	 window.pack();
    	 window.setVisible(true);
    	 
-   	 bluePoly = new BluePolymorph(50, 50);
-   	 redmorph=new RedMorph(100,100);
-   	 mmorph=new MovingMorph(200, 200);
+   	
    	 timer = new Timer(1000 / 30, this);
    	 timer.start();
+   	 
+   	 polymorph=new ArrayList<Polymorph>();
+   	 polymorph.add(new BluePolymorph(50, 50));
+   	 polymorph.add(new RedMorph(100, 100));
+   	 polymorph.add(new MovingMorph(150, 150));
+   	
+   	
     }
     
     public void paintComponent(Graphics g){
     //draw background
    	 g.setColor(Color.LIGHT_GRAY);
    	 g.fillRect(0, 0, 500, 500);
-   	
+   	for (int i = 0; i < polymorph.size(); i++) {
+		polymorph.get(i).draw(g);
+	}
    	 //draw polymorph
-   	 bluePoly.draw(g);
-   	 redmorph.draw(g);
-   	 mmorph.draw(g);
+   	                      
    	 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+    	for (int i = 0; i < polymorph.size(); i++) {
+			polymorph.get(i).update();
+		}
    	 repaint();
-   	 bluePoly.update();
-   	 redmorph.update();
-   	 mmorph.update();
+   	
    	 
    	 
     }
